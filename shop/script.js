5366e77217a83fd58b3c1f2a49f68291ef5e95de
KrishnaDass95 <krishdass95@gmail.com>
2023-04-01 21:39:54 +0530

    Using an API to render UI is amaze dude

--- shop/script.js
+++ shop/script.js
@@ -8,6 +8,64 @@
 //   image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
 //   rating: { rate: 3.9, count: 120 },
 // };
+const menSection = document.querySelector('.men');
+const womenSection = document.querySelector('women');
+const jewelerySection = document.querySelector("jewelery");
+const electronicsSection = document.querySelector("electronics");
+
+
+
+function renderUI(image, name, price, rating){
+  return `<div class="item">
+  <img src="${image}" alt="Item" />
+  <div class="info">
+    <div class="row">
+      <div class="price">$${price}</div>
+      <div class="sized">S,M,L</div>
+      <div class="name">${name}</div>
+    </div>
+    <div class="colors">
+      Colors:
+      <div class="row">
+        <div class="circle" style="background-color: #000"></div>
+        <div class="circle" style="background-color: #4938af"></div>
+        <div class="circle" style="background-color: #203d3e"></div>
+      </div>
+    </div>
+    <div class="row">Rating:${rating}</div>
+  </div>
+  <button id="addBtn">Add to Cart</button>
+</div>`
+}
+
+
+
+fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    let stringifiedData = JSON.stringify(data);
+    localStorage.setItem('products', stringifiedData);
+    data.forEach((item) => {
+    if(item.category = "men's clothing"){
+      let mensElement = renderUI(item.image, item.name, item.price, item.rating);
+      menSection.innerHTML += mensElement;
+    }
+    })
+
+
+
+
+
+
+
+
+
+  }).catch((error) => {
+    console.log('error message', error);
+  })
+
+
 
 console.log('shop page');
 const searchBarElement = document.getElementById('search-field');
