f853ecc3f3f21a8072afe844bf7d1824deaea739
KrishnaDass95 <krishdass95@gmail.com>
2023-04-01 18:21:36 +0530

    Add event listeners for all the major events and buttons

--- login/script.js
+++ login/script.js
@@ -1,1 +1,13 @@
 // Write your script here
+console.log('login page');
+const emailField = document.getElementById('email');
+const passwordField = document.getElementById('password');
+const submitElement = document.getElementById('submit');
+
+
+function login(){
+    console.log('logging in');
+}
+
+
+submitElement.addEventListener('click', login)
--- signup/script.js
+++ signup/script.js
@@ -1,1 +1,17 @@
 // Write your script here
+console.log('Sign up page');
+const submitButton = document.getElementById('submit');
+const firstNameField = document.getElementById('fname');
+const lastNameField = document.getElementById('lname');
+const emailField = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+
+function handleSignUp(){
+    console.log('signup button');
+    
+}
+
+
+
+submitButton.addEventListener('click', handleSignUp);
